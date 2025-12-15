0aa832b7f25ccf55767765cbd82290a58f930031
Balakrishna <uppmaha47@gmail.com>
2025-12-15 16:14:19 +0000

    create table modified

--- ddl/create_table.sql
+++ ddl/create_table.sql
@@ -1,18 +1,19 @@
 BEGIN
-  EXECUTE IMMEDIATE '
-    CREATE TABLE employees (
-      emp_id NUMBER PRIMARY KEY,
-      emp_name VARCHAR2(50),
-      created_at DATE DEFAULT SYSDATE
-    )
-  ';
+   EXECUTE IMMEDIATE '
+     CREATE TABLE employees (
+       emp_id NUMBER PRIMARY KEY,
+       emp_name VARCHAR2(100),
+       salary NUMBER,
+       created_date DATE DEFAULT SYSDATE
+     )
+   ';
 EXCEPTION
-  WHEN OTHERS THEN
-    IF SQLCODE = -955 THEN
-      NULL; -- table already exists
-    ELSE
-      RAISE;
-    END IF;
+   WHEN OTHERS THEN
+     IF SQLCODE = -955 THEN
+        NULL; -- table already exists, ignore
+     ELSE
+        RAISE;
+     END IF;
 END;
 /
 
